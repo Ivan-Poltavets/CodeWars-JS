35db8b1c0240d90300b70b0c263109c94573a185
Ivan Poltavets <72940045+Ivan-Poltavets@users.noreply.github.com>
2021-10-06 16:18:48 +0300

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,23 @@
+function getLength(arr){
+    var length = arr.length
+    return length
+  }
+  function getFirst(arr){
+    var firstElement = arr[0]
+    return firstElement
+  }
+  function getLast(arr){
+    var lastElement = arr[arr.length-1]
+    return lastElement
+  }
+  function pushElement(arr){
+    var el=1;
+    //push el to ar
+    arr.push(el)
+    return arr
+  }
+  function popElement(arr){
+    //pop an element from arr
+    arr.pop()
+    return arr
+  }
